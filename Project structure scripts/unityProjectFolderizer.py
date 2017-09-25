03a128905aedadb22a0eafef33238caf4b406061
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2017-09-25 21:49:02 +0300

    Consistent casing

--- Project structure scripts/unityProjectFolderizer.py
+++ Project structure scripts/unityProjectFolderizer.py
@@ -1,11 +1,11 @@
 import os 
 
 folderNames = ['Audio', 'Animations', 'Images', 'Materials', 'Plugins', 'Prefabs', 'Resources', 'Scripts', 'Scenes']
-dir_path = os.path.dirname(os.path.realpath(__file__))
-userInput = input('This will make {0} empty folders in {1}, write "y" to continue\n'.format(folderNames.__len__(), dir_path))
+dirPath = os.path.dirname(os.path.realpath(__file__))
+userInput = input('This will make {0} empty folders in {1}, write "y" to continue\n'.format(folderNames.__len__(), dirPath))
 if userInput == 'y' or userInput == 'Y' :
     for f in folderNames:
-        fullPath = dir_path + '\\' + f
+        fullPath = dirPath + '\\' + f
         print(fullPath)
         if not os.path.exists(fullPath):
             os.makedirs(f)
