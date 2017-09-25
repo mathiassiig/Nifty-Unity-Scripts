03c79cc8143a5c16b5b0b8a0fa3d7f38e0b23f9e
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2017-09-25 22:32:58 +0300

    Code style

--- Project structure scripts/unityProjectFolderizer.py
+++ Project structure scripts/unityProjectFolderizer.py
@@ -1,9 +1,20 @@
-import os 
+import os
 
-folderNames = ['Audio', 'Animations', 'Images', 'Materials', 'Plugins', 'Prefabs', 'Resources', 'Scripts', 'Scenes']
+folderNames = ['Audio',
+               'Animations',
+               'Images',
+               'Materials',
+               'Plugins',
+               'Prefabs',
+               'Resources',
+               'Scripts',
+               'Scenes']
+
 dirPath = os.path.dirname(os.path.realpath(__file__))
-userInput = input('This will make {0} empty folders in {1}, write "y" to continue\n'.format(folderNames.__len__(), dirPath))
-if userInput == 'y' or userInput == 'Y' :
+userInput = input(
+                'This will make {0} empty folders in {1}, write "y" to continue\n'
+                .format(folderNames.__len__(), dirPath))
+if userInput == 'y' or userInput == 'Y':
     for f in folderNames:
         fullPath = dirPath + '\\' + f
         print(fullPath)
